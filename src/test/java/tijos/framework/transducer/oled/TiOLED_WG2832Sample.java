bb994442508df04fdf5494bcb4beed4032bd247c
andy <andy.yang@tijos.net>
2018-09-26 16:10:12 +0800

    1.修正初始化序列。 2.完善例程。 3.增加实物图片说明。

--- src/test/java/tijos/framework/transducer/oled/TiOLED_WG2832Sample.java
+++ src/test/java/tijos/framework/transducer/oled/TiOLED_WG2832Sample.java
@@ -1,13 +1,42 @@
 package tijos.framework.transducer.oled;
 
+import java.io.IOException;
+
+import tijos.framework.devicecenter.TiI2CMaster;
+import tijos.framework.util.Delay;
+
 /**
- * Hello world!
+ * WG2832 Sample
  *
  */
 public class TiOLED_WG2832Sample
 {
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		try {
+			/*
+			 * TiI2CMaster port 0
+			 */
+			int i2cPort0 = 0;
+
+			/*
+			 * Open the I2C port 
+			 */
+			TiI2CMaster i2c0 = TiI2CMaster.open(i2cPort0);
+			
+			TiOLED_WG2832 wg2832 = new TiOLED_WG2832(i2c0, 0x3c);
+			
+			wg2832.turnOn();
+			wg2832.clear();
+			wg2832.print(0, 0, "abcdefghijklmnop");
+			wg2832.print(1, 0, "ABCDEFGHIJKLMNOP");
+			
+			while(true) {
+				Delay.msDelay(1000);
+			}
+		} catch (IOException ie) {
+			ie.printStackTrace();
+		}			
     }
 }
