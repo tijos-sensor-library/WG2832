6856bb64149b3ddf64188d558539ad0ea474b7da
lemon <lemon.yang@tijos.net>
2018-09-26 11:00:10 +0800

    Initial version

---
+++ src/main/java/tijos/framework/transducer/oled/TiOLED_WG2832.java
@@ -0,0 +1,13 @@
+package tijos.framework.transducer.oled;
+
+/**
+ * Hello world!
+ *
+ */
+public class TiOLED_WG2832 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/tijos/framework/transducer/oled/TiOLED_WG2832Sample.java
@@ -0,0 +1,13 @@
+package tijos.framework.transducer.oled;
+
+/**
+ * Hello world!
+ *
+ */
+public class TiOLED_WG2832Sample
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
